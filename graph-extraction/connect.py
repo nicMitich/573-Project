bda4590de3595638918788e5c208c39b53f884ec
Hrisika <hjagdeep@asu.edu>
2026-04-01 13:52:04 -0700

    deleting extraneous

--- graph-extraction/connect.py
+++
@@ -1,7 +0,0 @@
-from neo4j import GraphDatabase
-
-URI = "<database-uri>"
-AUTH = ("<username>", "<password>")
-
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
